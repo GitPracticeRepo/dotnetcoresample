23a393c3fca4efe05772e44a432ba01716eacab9
shaikkhajaibrahim <qtkhaja@gmail.com>
2020-05-15 14:11:03 +0000

    Added basic code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcoresample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Build for jenkins practice");
+        }
+    }
+}
